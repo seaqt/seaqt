12209a6e14b9cdad581c7906cd24c07cd87c1068
Jacek Sieka <arnetheduck@gmail.com>
2026-07-08 21:51:40 +0200

    update bindings

---
+++ seaqt/QtWebSockets/QtWebSockets.cpp
@@ -0,0 +1,5 @@
+#include "gen_qmaskgenerator.cpp"
+#include "gen_qwebsocket.cpp"
+#include "gen_qwebsocketcorsauthenticator.cpp"
+#include "gen_qwebsocketprotocol.cpp"
+#include "gen_qwebsocketserver.cpp"
